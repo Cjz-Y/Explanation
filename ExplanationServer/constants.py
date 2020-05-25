8d3b55c2f1b589546b758f631f1afa249c799eaf
cjz.y <cjz.y@hotmail.com>
2020-05-25 17:45:41 +0800

    merge cyl lyx

--- ExplanationServer/constants.py
+++ ExplanationServer/constants.py
@@ -1,7 +1,7 @@
 from interpreter.lime.lib.predict import Predict
 
 LIME_EXPLANATION = {
-    '10': Predict(),
-    '5': Predict(),
-    '2': Predict()
+    '10': Predict(num_features=10),
+    '5': Predict(num_features=5),
+    '2': Predict(num_features=2)
 }
